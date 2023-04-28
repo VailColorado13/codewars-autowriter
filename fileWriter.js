baaa8f1241f9c204bb007e49fe7d0c3bf669534d
Matt Vail <mattvailco@gmail.com>
2023-04-27 18:22:35 -0600

    fixed issue of fs.writeFile executing before fs.mkdir

--- fileWriter.js
+++ fileWriter.js
@@ -6,16 +6,21 @@ module.exports = {
     writeFolder: (kyu, title, link) => {
         const dirName = dataProcessor.createDirName(kyu, title)
         fs.mkdir(`../${dirName}`, (err) => {
-            if (err) {return console.error(err);}
-            console.log('\n\n\nDirectory created successfully!\n')
-            console.log('navigate up one folder: cd ..')
-            console.log('\n' + 'cd ' + dirName + '\n\n\n')
-        })
-        fs.writeFile(`../${dirName}/solution.js`, textWriter.writeHeader(kyu, title, link), (err) => {
+          if (err) {
+            return console.error(err)
+          }
+          console.log('\n\n\nDirectory created successfully!\n')
+          console.log('navigate up one folder: cd ..')
+          console.log('\n' + 'cd ' + dirName + '\n\n\n')
+      
+          // move the writeFile function here
+          fs.writeFile(`../${dirName}/solution.js`, textWriter.writeHeader(kyu, title, link), (err) => {
             if (err) console.log(err)
+          })
         })
+      }
 
     }
    
-}
+
 
